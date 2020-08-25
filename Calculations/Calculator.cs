76c9a968389d3a26730e45f540c8c33d0baa97bd
Caleb Morales <clbmrls@gmail.com>
2020-08-25 10:31:49 -0600

    Added more functions

--- Calculations/Calculator.cs
+++ Calculations/Calculator.cs
@@ -4,17 +4,64 @@ namespace Calculations
 {
     public class Calculator
     {
-        public static int TimeToTravel(double distance, int speed)
+        public static int SovleForTime(double distance, int speed)
         {
-            return (int) Math.Round((distance / speed) * 60);
+            if (distance < 0 | speed <= 0)
+            {
+                distance = 0;
+                speed = 1;
+            }
+            return (int) Math.Round((distance / speed) * 60); // Returns X minutes it will take to travel Y distance at Z speed
+        }
+
+        public static double SolveForTime(double speed, double minutes)
+        {
+            if (speed < 0 | minutes < 0)
+            {
+                speed = 0;
+                minutes = 0;
+            }
+            return Math.Round(((speed * minutes) / 60), 2);
+        }
+
+        public static double SolveForSpeed(double distance, double minutes)
+        {
+            if (distance < 0 | minutes <= 0)
+            {
+                distance = 0;
+                minutes = 1;
+            }
+            return Math.Round(((distance / minutes) * 60), 2);
         }
 
         public static string TimeToString(double travelTime)
         {
+            if (travelTime < 0)
+            {
+                travelTime = 0;
+            }
             int minutes = (int) travelTime;
             int hours = (int) travelTime / 60;
             minutes = (int)minutes % 60;
             return hours + " Hours, " + minutes + " Minutes";
+        }
+
+        public static double MilesToKilometers(double miles)
+        {
+            if (miles < 0)
+            {
+                miles = 0;
+            }
+            return Math.Round((miles * 1.60934), 2);
+        }
+
+        public static double KilometersToMiles(double kilometers)
+        {
+            if (kilometers < 0)
+            {
+                kilometers = 0;
+            }
+            return Math.Round((kilometers / 1.60934), 2);
         }
     }
 }
--- Tests/CalculatorTest.cs
+++ Tests/CalculatorTest.cs
@@ -15,8 +15,9 @@ namespace Tests
         {
             int distancePerHour = 45;
             double distance = 26.2;
-            double travelTime = Calculator.TimeToTravel(distance, distancePerHour);
-            Assert.AreEqual(35, travelTime);
+            Assert.AreEqual(35, Calculator.SovleForTime(distance, distancePerHour));
+            distance = -0.1;
+            Assert.AreEqual(0, Calculator.SovleForTime(distance, distancePerHour));
         }
 
         [Test]
@@ -24,10 +25,54 @@ namespace Tests
         {
             int distancePerHour = 30;
             double distance = 56.7;
-            double travelTime = Calculator.TimeToTravel(distance, distancePerHour);
+            double travelTime = Calculator.SovleForTime(distance, distancePerHour);
             Assert.AreEqual(113, travelTime);
-            string travelTimeString = Calculator.TimeToString(travelTime);
-            Assert.AreEqual("1 Hours, 53 Minutes", travelTimeString);
+            Assert.AreEqual("1 Hours, 53 Minutes", Calculator.TimeToString(travelTime));
+        }
+
+        [Test]
+        public void milesToKilometers()
+        {
+            double miles = 26.2;
+            Assert.AreEqual(42.16, Calculator.MilesToKilometers(miles));
+            miles = 33.6;
+            Assert.AreEqual(54.07, Calculator.MilesToKilometers(miles));
+            miles = -17.2;
+            Assert.AreEqual(0, Calculator.MilesToKilometers(miles));
+        }
+
+        [Test]
+        public void kilometersToMiles()
+        {
+            double kilometers = 26.2;
+            Assert.AreEqual(16.28, Calculator.KilometersToMiles(kilometers));
+            kilometers = 33.6;
+            Assert.AreEqual(20.88, Calculator.KilometersToMiles(kilometers));
+            kilometers = -44.44;
+            Assert.AreEqual(0, Calculator.MilesToKilometers(kilometers));
+        }
+
+        [Test]
+        public void distanceOverTime()
+        {
+            double speed = 45;
+            double minutes = 32;
+            Assert.AreEqual(24, Calculator.SolveForTime(speed, minutes));
+            speed = 66;
+            minutes = 33;
+            Assert.AreEqual(36.3, Calculator.SolveForTime(speed, minutes));
+            minutes = -16.2;
+            Assert.AreEqual(0, Calculator.SolveForTime(speed, minutes));
+        }
+
+        [Test]
+        public void solveForSpeed()
+        {
+            double distance = 26.2;
+            int minutes = 60;
+            Assert.AreEqual(26.2, Calculator.SolveForSpeed(distance, minutes));
+            minutes = 0;
+            Assert.AreEqual(0, Calculator.SolveForSpeed(distance, minutes));
         }
     }
 }
